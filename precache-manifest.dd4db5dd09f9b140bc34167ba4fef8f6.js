8c29719fb1355a7214398e9cbd9589845db5a46c
gunratnamore911 <gunratnamore@gmail.com>
2020-08-10 12:29:48 +0530

    Updates

---
+++ precache-manifest.dd4db5dd09f9b140bc34167ba4fef8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3aee90815961c6d9dc14375d3b5372",
+    "url": "/React-js-amazon-clone/index.html"
+  },
+  {
+    "revision": "37573287c1025c269c44",
+    "url": "/React-js-amazon-clone/static/css/main.0a0daf5f.chunk.css"
+  },
+  {
+    "revision": "d5d58f34a9d15516aa0a",
+    "url": "/React-js-amazon-clone/static/js/2.914a759c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/React-js-amazon-clone/static/js/2.914a759c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37573287c1025c269c44",
+    "url": "/React-js-amazon-clone/static/js/main.887d655b.chunk.js"
+  },
+  {
+    "revision": "152fa8cc95633dcc2f90",
+    "url": "/React-js-amazon-clone/static/js/runtime-main.3237f0ef.js"
+  }
+]);
